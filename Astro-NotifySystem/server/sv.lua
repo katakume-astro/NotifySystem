d92457978a8bf467279ed01a54d6830ad70decda
katakume <72819680+katakume-astro@users.noreply.github.com>
2022-10-13 21:57:42 +0200

    little ac

---
+++ Astro-NotifySystem/server/sv.lua
@@ -0,0 +1,6 @@
+RegisterNetEvent('astro:cheater')
+AddEventHandler('astro:cheater', function()
+local source = source
+local sourceXPlayer = ESX.GetPlayerFromId(source)
+print(('[^3UWAGA:^7] Gracz ^5%s^7 uzywa prawdopodobnie cheatow!'):format(sourceXPlayer.source))
+end)
