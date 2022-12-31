1046eb05fda66c428b7c7d32e65306714d6ade82
katakume <72819680+katakume-astro@users.noreply.github.com>
2022-12-31 17:36:22 +0100

    Update sv.lua
    xd

--- Astro-NotifySystem/server/sv.lua
+++ Astro-NotifySystem/server/sv.lua
@@ -3,6 +3,6 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent('astro:cheater')
 AddEventHandler('astro:cheater', function()
-local sourceXPlayer = ESX.GetPlayerFromId(source)
-print(('[^3UWAGA:^7] Gracz ^5%s^7 uzywa prawdopodobnie cheatow!'):format(sourceXPlayer.source))
+local xPlayer = ESX.GetPlayerFromId(source)
+xPlayer.kick('[ASTRO] Stop')
 end)
